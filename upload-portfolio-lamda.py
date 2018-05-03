93b78864353741f82274e8ff281430d2f999d2eb
nav7 <navshergill88@gmail.com>
2018-05-03 18:05:36 -0400

    Add upload-portfolio script to repo

---
+++ upload-portfolio-lamda.py
@@ -0,0 +1,43 @@
+import boto3
+from botocore.client import Config
+import StringIO
+import zipfile
+import mimetypes
+
+def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:026091294828:deployPortfolioTopic')
+
+    location = {
+        "bucketName" : 'navportfolio.info',
+        "objectKey" : 'portfoliobuild.zip'
+    }
+    try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3location"]
+        print "Building portfolio location from" + str(location)
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        build_bucket = s3.Bucket('build.navportfolio.info')
+        portfolio_bucket = s3.Bucket(location["bucketName"])
+
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print 'Job done!'
+        topic.publish(Subject='Portfolio Deployed: Success', Message = 'Your Portfolio has been deployed successfully!')
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+    except:
+        topic.publish(Subject='Portfolio Deployed: Failed', Message = 'Your Portfolio has been deployed failed!')
+        raise
+    return 'Hello from Lambda'
